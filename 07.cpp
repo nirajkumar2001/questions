6d477e56a4e7d0be82a5d4efbdd65eeeca619266
nirajkumar2001 <113297572+nirajkumar2001@users.noreply.github.com>
2024-01-23 19:24:57 +0530

    Create 07.cpp

---
+++ 07.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int reverse(int x) {
+      int rev=0;
+      while(x !=0){
+        int m=x%10;
+        x=x/10;
+        if(rev>INT_MAX/10||rev<INT_MIN/10){return 0;}
+
+        rev=(rev*10)+m;
+      }
+      return rev;
+    }
+      
+      
+      
+    
+      
+        
+    
+};
