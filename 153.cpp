be394d483674bcbcbfdc99ffe2bb3cf2e3b72af6
nirajkumar2001 <113297572+nirajkumar2001@users.noreply.github.com>
2024-01-24 18:26:32 +0530

    leetcode153.cpp

---
+++ 153.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int findMin(vector<int>& arr) {
+        int low=0,high=arr.size()-1, ans=INT_MAX;
+	while(low<=high)
+	{
+		int mid=(low+high)/2;
+        if (arr[low] <= arr[high]) {
+            ans = min(ans, arr[low]);
+            break;
+        }
+		if(arr[low]<=arr[mid])
+		{
+			ans=min(ans,arr[low]);
+			low=mid+1;
+		}
+		else{
+			ans=min(ans,arr[mid]);
+			high=mid-1;
+		}
+	}
+	return ans;
+}
+        
+    
+};
